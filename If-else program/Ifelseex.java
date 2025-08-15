8ef8b0bc65c6bbe555bb8ea5289939857c54e4fe
Shrit kumar <shritnayak2005@gmail.com>
2025-08-15 17:27:29 +0530

    Add files via upload

---
+++ If-else program/Ifelseex.java
@@ -0,0 +1,31 @@
+package infosys_internship;
+
+public class Ifelseex 
+{
+
+	public static void main(String[] args) 
+	{
+		int num = -7;
+        
+		
+		if (num > 0) 
+        {
+            
+        	System.out.println("The number is positive.");
+        } 
+        
+		else if (num < 0) 
+		{
+            
+			System.out.println("The number is negative.");
+        } 
+        
+        else 
+        {
+            
+        	System.out.println("The number is zero.");
+        }
+
+	}
+
+}
