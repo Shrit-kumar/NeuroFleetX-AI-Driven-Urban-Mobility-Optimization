be4d58e48f67a39d01fe4f36221c1f215ea85f96
Shrit kumar <shritnayak2005@gmail.com>
2025-08-15 17:14:24 +0530

    Add files via upload

---
+++ Datatypes and its variable/Datatypes_and_Variables.java
@@ -0,0 +1,32 @@
+package infosys_internship;
+
+public class Datatypes_and_Variables {
+
+	public static void main(String[] args) {
+		
+		String firstName = "Shrit";
+		String space=" ";
+		String lastName = "kumar";
+		String fullName = firstName + space +lastName;
+		System.out.println(fullName);
+		
+		int num1=5;
+		int num2=5;
+		System.out.println(num1+num2);
+		
+		float f1=3.5f;
+		float f2=3.5f;
+		System.out.println(f1*f2);
+		
+		char c1='c';
+		char c2='a';
+		char c3='t';
+		System.out.println("" + c1 + c2 + c3);
+		
+		boolean b=true;
+		System.out.println(b);
+
+
+	}
+
+}
